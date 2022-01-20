9b0afe813aa774467bb748759a6a830b043aaa31
Suan <niangsthang@gmail.com>
2022-01-19 19:57:18 -0500

    change makeImages and main file to origin

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function(e){
     e.preventDefault();
     const searchTerm=form.elements.search.value;
     const config= { params: {q: searchTerm, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}}
     const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, config)
-    console.log(res.data);
-})
+    let  gifs= res.data.data;
+    makeImages(gifs)
+    //let arrayIndex= Math.floor(Math.random()*res.data.length)
 
+}
+)
+//let gifs=res.data.data;
 const makeImages = (gifs) => {
-    for (let result of gifs){
-        if(result.data.images){
-            const img = document.createElement('IMG');
-            img.src= result.data.images.original;
-            document.body.append(img)
-        }
+    for(let result of gifs){
+        const img = document.createElement('IMG')
+        img.src = result.images.original.url;
+        document.body.append(img)
     }
 }
+    
+
+
+ 
+
+
+
