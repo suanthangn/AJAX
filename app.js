6b379496d151f4aee2178e2c8646f14e6724afe6
Suan <niangsthang@gmail.com>
2022-01-21 01:34:37 -0500

    remove button now functional

--- app.js
+++ app.js
@@ -4,22 +4,16 @@ form.addEventListener('submit', async function(e){
     const searchTerm=form.elements.search.value;
     const config= { params: {q: searchTerm, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}}
     const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, config)
-    //let  gifs= (res.data.data)
-    //makeImages(gifs)
-    //let arrayIndex= Math.floor(Math.random()*res.data.length)
     index = Math.floor(Math.random()*res.data.data.length)
     let gif = (res.data.data[index]);
-    //console.log(gif)
-    //makeImages(gif);
     const img = document.createElement('IMG')
     img.src=gif.images.original.url;
     $gifArea.append(img);
 }
 )
 const $gifArea = $("#image-placeholder");
-
-
-$("#remove").on("click", function() {
+$("#remove").on("click", function(e) {
+    e.preventDefault();
     $gifArea.empty();
   });
 
